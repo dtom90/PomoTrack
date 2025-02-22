5904e8b1ab20b6307dee15cdfd15e6cde5557bc7
David Thomason <dtom90@users.noreply.github.com>
2025-02-22 18:37:47 -0500

    more babel stuff

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,17 @@
 module.exports = {
   presets: [
-    '@vue/app'
+    ['@vue/app', {
+      useBuiltIns: 'entry',
+      polyfills: [
+        'es.promise',
+        'es.symbol'
+      ]
+    }]
   ],
   plugins: [
     '@babel/plugin-proposal-class-properties',
-    '@babel/plugin-proposal-optional-chaining'
+    '@babel/plugin-proposal-optional-chaining',
+    '@babel/plugin-proposal-private-methods',
+    '@babel/plugin-proposal-private-property-in-object'
   ]
 }
