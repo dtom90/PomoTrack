d74570a0e77fa12d7d04b217ad00c793d23b101e
David Thomason <911671+dtom90@users.noreply.github.com>
2025-05-02 20:20:56 -0500

    rendering more

--- vue3/PomoTrack/src/main.ts
+++ vue3/PomoTrack/src/main.ts
@@ -4,29 +4,29 @@ import './styles/main.scss';
 import store from './store/index.js'
 
 // Bootstrap
-import './lib/bootstrap'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
 
 // Font Awesome Icons
 import { FontAwesomeIcon } from './lib/font-awesome-icons.js'
 
+import packageInfo from '../package.json'
+
 import { createApp } from 'vue'
 import { createBootstrap } from 'bootstrap-vue-next'
+
 import App from './components/App.vue'
 
 const app = createApp(App)
+
+app.config.globalProperties.$appVersion = packageInfo.version
 
 app.component('font-awesome-icon', FontAwesomeIcon)
 app.use(store)
 app.use(createBootstrap())
 app.mount('#app')
 
-// import Vue from 'vue'
-// import App from './components/App.vue'
-// import packageInfo from '../package.json'
-
 // Vue.config.productionTip = false
-
-// Vue.prototype.$appVersion = packageInfo.version
 
 // async function initApp () {
 //   const app = new Vue({
